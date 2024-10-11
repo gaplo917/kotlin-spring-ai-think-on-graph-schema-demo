75736cbbe6a75cb4c593d550edc3a055b84b1af4
Gary Lo <logap@me.com>
2024-10-11 20:00:11 +0800

    initial

---
+++ src/main/kotlin/com/gaplo917/demo/KotlinSpringAiGraphSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.gaplo917.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringAiGraphSearchApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringAiGraphSearchApplication>(*args)
+}
---
+++ src/test/kotlin/com/gaplo917/demo/KotlinSpringAiGraphSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gaplo917.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringAiGraphSearchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
